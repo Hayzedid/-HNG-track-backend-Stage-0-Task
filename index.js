0f078679eef2644fda3e4bb8520ef225fff8bfa5
Hayzedid <iazeez775@gmail.com>
2025-10-18 12:08:00 +0200

    Port configuration

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
